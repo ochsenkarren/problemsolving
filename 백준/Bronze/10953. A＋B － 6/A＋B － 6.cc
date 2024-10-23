fc8f19ff60a45a3dc6b25e143a14717816472c81
ochsenkarren <probablecode@gmail.com>
2024-10-23 23:15:48 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,10 @@
+#include <cstdio>
+int main(void) {
+    int T, a, b;
+    scanf("%d", &T);   
+    for (int i = 0; i < T; i++) {
+        scanf("%d,%d", &a, &b);   
+        printf("%d\n", a + b);
+    }
+    return 0;
+}
