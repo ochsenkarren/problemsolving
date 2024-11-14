2a230f1127109927483c6a73788c50e3c9c51e97
ochsenkarren <probablecode@gmail.com>
2024-11-14 10:02:11 +0900

    [level 3] Title: 정수 삼각형, Time: 0.64 ms, Memory: 8.55 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int N;
+int toI(int n, int k) { return (n * (n + 1)) / 2 + k; }
+
+int solution(vector<vector<int>> tri) {
+    N = tri.size();
+    vector<int> v((N * (N + 1)) / 2, 0);
+    v[0] = tri[0][0];
+    for (int i = 1; i < N; ++i) {
+        v[toI(i, 0)] = tri[i][0] + v[toI(i - 1, 0)];
+        v[toI(i, i)] = tri[i][i] + v[toI(i - 1, i - 1)];
+    }
+    for (int i = 2; i < N; ++i)
+        for (int j = 1; j < i; ++j)
+            v[toI(i, j)] = max(v[toI(i - 1, j)], v[toI(i - 1, j - 1)]) + tri[i][j];
+    int answer = 0;
+    for (int i = (N * (N - 1)) / 2; i < (N * (N + 1)) / 2; ++i)
+        answer = max(answer, v[i]);
+    return answer;
+}
+    // (n, k) -> (n * (n + 1)) / 2 + k;
+    // 왼쪽 아래로 (n, k) -> (n + 1, k)
+    // 오른쪽 아래로 (n, k) -> (n + 1, k + 1)
+    // 왼쪽 위로 (n, k) -> (n - 1, k - 1)
+    // 오른쪽 위로 (n, k) -> (n - 1, k)
